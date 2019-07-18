03e4141f164e8e80af2901ed6445494f4880e9a3
nazrul-avash <nazrulavash@gmail.com>
2019-07-18 17:05:03 +0600

    includes script for decluttering directory

---
+++ Boring_files_mover.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os,shutil
+src = "/home/nazrul/Pictures/"
+dest = "/home/nazrul/Downloads/Screenshots/"
+genOb = os.walk(src)
+file_num= 1
+for d in genOb:
+	for f in d[2]:
+		if "Screenshot" in f:
+			shutil.copy(d[0]+"/"+f,dest)
+			os.unlink(d[0]+"/"+f)
+		temp = os.path.splitext(f)
+		print(d[0]+"wall"+str(file_num)+temp[1]+" <<<<<<  "+d[0]+f)	
+		os.rename(d[0]+f,d[0]+"wall"+str(file_num)+temp[1])
+		file_num += 1	
