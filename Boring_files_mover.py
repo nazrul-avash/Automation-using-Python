84bfee2251c3ab3b38c7a160910f8692e6978138
nazrul-avash <nazrulavash@gmail.com>
2019-08-21 08:09:54 +0600

    fixes file bug

--- Boring_files_mover.py
+++ Boring_files_mover.py
@@ -2,13 +2,15 @@
 import os,shutil
 src = "/home/nazrul/Pictures/"
 dest = "/home/nazrul/Downloads/Screenshots/"
-genOb = os.walk(src)
+genOb = os.walk(src) 
 file_num= 1
 for d in genOb:
 	for f in d[2]:
 		if "Screenshot" in f:
-			shutil.copy(d[0]+"/"+f,dest)
+			print(f)
+			shutil.copy(d[0]+"/"+f,dest) #
 			os.unlink(d[0]+"/"+f)
+			continue
 		temp = os.path.splitext(f)
 		print(d[0]+"wall"+str(file_num)+temp[1]+" <<<<<<  "+d[0]+f)	
 		os.rename(d[0]+f,d[0]+"wall"+str(file_num)+temp[1])
